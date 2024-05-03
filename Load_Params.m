4bd10ebf2d7085ff517fa7fc8534b9ff199e070d
Edoardo Cavallotti <91212793+edocava@users.noreply.github.com>
2024-05-03 11:35:02 +0200

    Files Uploaded

---
+++ Load_Params.m
@@ -0,0 +1,33 @@
+peak_pw = 150e3;
+torque_max = 310;
+rpm_max = 16e3;
+
+tau_g = 10.5/1;
+pt_del = 20e-3;
+tau_torque = 50e-3;
+motor_eff = .9;
+mech_eff = .95;
+torsional_stiff = 9e3;
+batt_cap = 58e3;
+batt_V = 800;
+f0 = 0.009;
+f2 = 6.5e-6;
+
+deadtime = 20e-3;
+risetime = 25e-3;
+
+kerb_w = 1812;
+wheelbase = 2.77;
+cog = .55; % center of gravity
+
+Cx = .27;
+Af = 2.36;
+
+wheel_width = 215e-3; % h del cilindro
+wheel_prof = 0.5*wheel_width; % dist of internal radius from external radius
+rim_size = 19*2.54*e-2;% internal radius
+wheel_mass = 9.4; %average passenger car mass
+
+
+
+
