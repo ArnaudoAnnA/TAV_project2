d88726e3d0326e094ec4314d05a0635a8413c5d7
Edoardo Cavallotti <91212793+edocava@users.noreply.github.com>
2024-06-09 11:31:13 +0200

    Braking separate models added

--- MAIN2.m
+++ MAIN2.m
@@ -7,5 +7,7 @@ Load_Tyre_Data
 % override
 % velstart = 150/3.6; % [m/s]
 
-open("Tyre_Pac96_braking.slx")
-sim("Tyre_Pac96_braking.slx")
+% open("Tyre_Pac96_braking.slx")
+% sim("Tyre_Pac96_braking.slx")
+open("Tyre_Pac96_braking_std.slx")
+sim("Tyre_Pac96_braking_std.slx")
